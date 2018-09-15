68c2ba19154938971cffdf1dab894f603122fbe1
chuanqingli <9915252@qq.com>
2018-09-15 09:30:17 +0800

    修改：     arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -11,14 +11,19 @@ color(){
     esac
 }
 
-wget https://www.archlinux.org/mirrorlist/?country=CN&use_mirror_status=on aaa.txt
+color red "wget mirrorlist and update!"
+
+wget https://www.archlinux.org/mirrorlist/\?country=CN\&use_mirror_status=on -O aaa.txt
 sed -i 's/#Server/Server/g' aaa.txt
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 mv aaa.txt /etc/pacman.d/mirrorlist
 chmod +r /etc/pacman.d/mirrorlist
 
+
+color red "cfdisk and format!"
 fdisk -l
-fdisk /dev/sda
+cfdisk
+#fdisk /dev/sda
 
 mkfs.ext4 /dev/sda1
 mkfs.ext4 /dev/sda3
@@ -26,24 +31,33 @@ mkfs.ext4 /dev/sda3
 mkswap /dev/sda2
 swapon /dev/sda2
 
+color red "mount!"
+
 mount /dev/sda1 /mnt
 
 mkdir /mnt/home
 mount /dev/sda3 /mnt/home
 
-pacstrap /mnt base base-devel --force
+color red "pacstrap!"
+pacstrap -i /mnt base base-devel gvim
 genfstab -U -p /mnt > /mnt/etc/fstab
 
+color red "arch-chroot!"
 arch-chroot /mnt
 
+
+
+color red "zone and time update!"
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 hwclock --systohc --utc
 
+color red "chuanqing!"
 echo chuanqing>/etc/hostname
 
 useradd -m -g wheel chuanqing
 passwd chuanqing
 
+color red "pacman!"
 pacman -S fcitx-im fcitx-configtool xorg xorg-xinit xfce4 grub
 
 grub-install --recheck /dev/sda
