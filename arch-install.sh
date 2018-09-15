2a7c4e45a7eba0474ab0a46aad9d97446746c817
chuanqingli <9915252@qq.com>
2018-09-15 11:59:08 +0800

    修改：     arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -19,6 +19,9 @@ mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 mv aaa.txt /etc/pacman.d/mirrorlist
 chmod +r /etc/pacman.d/mirrorlist
 
+echo "[archlinuxcn]">/etc/pacman.conf
+echo "SigLevel = Optional TrustAll">/etc/pacman.conf
+echo "Server = http://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch">/etc/pacman.conf
 
 color red "cfdisk and format!"
 fdisk -l
@@ -39,7 +42,8 @@ mkdir /mnt/home
 mount /dev/sda3 /mnt/home
 
 color red "pacstrap!"
-pacstrap -i /mnt base base-devel gvim
+pacstrap -i /mnt base base-devel gvim fcitx-im fcitx-configtool xorg xorg-xinit xfce4 grub lxdm  google-chrome wps-office
+
 genfstab -U /mnt > /mnt/etc/fstab
 
 color red "arch-chroot!"
@@ -55,12 +59,17 @@ echo chuanqing>/etc/hostname
 useradd -m -g wheel chuanqing
 passwd chuanqing
 
-color red "pacman!"
-pacman -S fcitx-im fcitx-configtool xorg xorg-xinit xfce4 grub
+color red "locale!"
+echo "zh_CN.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "export LANG=zh_CN.UTF-8">/etc/locale.conf
+echo "export LANGUAGE=zh_CN:en_US">/etc/locale.conf
+echo "export XMODIFIERS=@im=fcitx">/etc/locale.conf
 
 grub-install --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable dhcpcd.service
+systemctl enable lxdm.service
 
 exit
 reboot
